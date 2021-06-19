f5348fca8a577d13d73f71385cc22247a3cdaa70
prasoonjha <prasoonjha69@gmail.com>
2021-06-20 02:34:02 +0530

    add semantic-ui-react

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from 'semantic-ui-react';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Revents</h1>
+      <Button icon='facebook f' circular color='blue'/>
     </div>
   );
 }
